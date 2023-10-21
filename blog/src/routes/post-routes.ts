91baf940e7d3167fb5783b6d43172b6388dac00f
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2023-10-21 17:17:37 -0300

    remove unused things

--- blog/src/routes/post-routes.ts
+++ blog/src/routes/post-routes.ts
@@ -9,27 +9,22 @@ import {
 
 export const router = Router()
 
-router.get('/',async (req, res) => {
+router.get('/', async (req, res) => {
   const posts = await listAll()
   return res.status(200).json(posts)
 })
 
-router.get('/',async (req, res) => {
+router.post('/', async (req, res) => {
   const post = await createPost(req.body)
   return res.status(200).json(post)
 })
 
-router.post('/',async (req, res) => {
-  const post = await createPost(req.body)
-  return res.status(200).json(post)
-})
-
-router.put('/:id/publish',async (req, res) => {
+router.put('/:id/publish', async (req, res) => {
   await publishPost(req.params.id)
   return res.status(201).send()
 })
 
-router.delete('/:id',async (req, res) => {
+router.delete('/:id', async (req, res) => {
   await deletePost(req.params.id)
   return res.status(200).send()
 })
